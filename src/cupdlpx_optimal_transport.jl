2453cc68b5b3ad9f4f519e6c9412c1eae3078a0e
Azamat Berdyshev <aberdysh@gmail.com>
2025-12-20 19:13:27 -0500

    refine cuPDLPx

--- src/cupdlpx_optimal_transport.jl
+++ src/cupdlpx_optimal_transport.jl
@@ -1,52 +1,52 @@
-using CUDA
-using CUDA.CUSPARSE
+using cuPDLPx.LibcuPDLPx
 
 function form_optimal_transport_lp(
-    cost::CuMatrix{Float32}, μ::CuVector{Float32}, ν::CuVector{Float32}
+    costs::CuMatrix{Float32}, μ::Vector{Float64}, ν::Vector{Float64}
 )
-    (n, m) = Int32.(size(cost))
+    (n, m) = Int32.(size(costs))
     @assert length(μ) == n
     @assert length(ν) == m
     num_vars = n * m
     num_cons = n + m
-    nnz = 2 * num_vars
 
     # objective coefficients
-    c = vec(cost)  # CuVector view, no copy
-
-    # CSR structure: num_cons x num_vars, nnz = 2 * num_vars
-    # row pointers on CPU (tiny), then copy to GPU
-    rowptr = Vector{Int32}(undef, num_cons + 1)
-    rowptr[1] = one(Int32)
-    @inbounds for i in 1:n
-        rowptr[i+1] = rowptr[i] + m
-    end
-    @inbounds for j in 1:m
-        rowptr[n+j+1] = rowptr[n+j] + n
-    end
-
-    colval = CuVector{Int32}(undef, nnz)
-    one_to_num_vars = CuVector(Int32(1):Int32(num_vars))
-    # source constraints block (rows 1..n): row-wise access in column-major flattening
-    @. colval[one_to_num_vars] = linear_index(one_to_num_vars, n, m)
-    # target constraints block (rows n+1..n+m): contiguous columns
-    # colval[(num_vars+1):(2 * num_vars)] = 1:num_vars
-    colval[(num_vars+1):end] .= one_to_num_vars
-
-    # values are all ones
-    nzval = CUDA.ones(Float32, nnz)
-    A = CuSparseMatrixCSR(CuArray(rowptr), colval, nzval, (num_cons, num_vars))
+    c = Vector{Float64}(undef, num_vars)
+    copyto!(c, vec(costs))
+    # constant term in objective
+    c₀ = Float64[0.0]
 
     # equality constraints: con_lb == con_ub == [μ; ν]
-    rhs = vcat(μ, ν)
+    rhs = [μ; ν]
     con_lb = rhs
     con_ub = copy(rhs)
 
     # variable bounds: x >= 0
-    var_lb = CUDA.zeros(Float32, num_vars)
-    var_ub = CUDA.fill(Inf32, num_vars)
-    lp_probelm = cuPDLPx.LinearProgrammingProblem(c, A, con_lb, con_ub, var_lb, var_ub)
-    return lp_probelm
+    var_lb = zeros(Float64, num_vars)
+    var_ub = fill(Inf64, num_vars)
+
+    # CSR structure: num_cons x num_vars, nnz = 2 * num_vars
+    nnz = Int32(2 * num_vars)
+    row_ptr = Vector{Int32}(undef, num_cons + 1)
+    row_ptr[1] = one(Int32)
+    @inbounds for i in 1:n
+        row_ptr[i+1] = row_ptr[i] + m
+    end
+    @inbounds for j in 1:m
+        row_ptr[n+j+1] = row_ptr[n+j] + n
+    end
+
+    col_ind = Vector{Int32}(undef, nnz)
+    one_to_num_vars = Int32(1):Int32(num_vars)
+    # source constraints block (rows 1..n): row-wise access in column-major flattening
+    @. col_ind[one_to_num_vars] = linear_index(one_to_num_vars, n, m)
+    # target constraints block (rows n+1..n+m): contiguous columns
+    # col_ind[(num_vars+1):(2 * num_vars)] = 1:num_vars
+    col_ind[(num_vars+1):end] .= one_to_num_vars
+
+    # values are all ones
+    vals = ones(Float64, nnz)
+    coefficients = (; c, con_lb, con_ub, var_lb, var_ub, c₀, nnz, row_ptr, col_ind, vals)
+    return coefficients
 end
 
 function linear_index(k::Int32, n::Int32, m::Int32)
