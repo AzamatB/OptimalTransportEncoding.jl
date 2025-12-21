e0b1110e999657e5b750e855c3f36a9b75e83b1f
Azamat Berdyshev <aberdysh@gmail.com>
2025-12-21 00:07:32 -0500

    add polish feasibility flag

--- src/optimal_transport.jl
+++ src/optimal_transport.jl
@@ -81,7 +81,8 @@ function solve_lp_problem(
     row_ptr::Vector{Int32},
     col_ind::Vector{Int32},
     vals::Vector{Float64},
-    termination_evaluation_frequency::Int32=Int32(8000)
+    termination_evaluation_frequency::Int32=Int32(8000),
+    polish_feasibility::Bool=false
 )
     A_desc_ref = Ref{Lib.matrix_desc_t}()
     params_ref = Ref{Lib.pdhg_parameters_t}()
@@ -104,11 +105,14 @@ function solve_lp_problem(
         params_ptr = Base.unsafe_convert(Ptr{Lib.pdhg_parameters_t}, params_ref)
         Lib.set_default_parameters(params_ptr)
 
-        # Set minimum iterations (check termination only every 8000 steps)
-        # termination_evaluation_frequency is field 6
+        # Set minimum iterations via setting termination_evaluation_frequency (field 6)
         offset = fieldoffset(Lib.pdhg_parameters_t, 6)
         freq_ptr = Ptr{Int32}(UInt(params_ptr) + offset)
         unsafe_store!(freq_ptr, termination_evaluation_frequency)
+        # Set polish feasibility flag (field 9)
+        offset = fieldoffset(Lib.pdhg_parameters_t, 9)
+        feasibility_ptr = Ptr{Bool}(UInt(params_ptr) + offset)
+        unsafe_store!(feasibility_ptr, polish_feasibility)
 
         lp_problem = Lib.create_lp_problem(
             pointer(c),
